27eae4c1a787a928ec2522c7189d4f682cb4525b
smdubarry <50964274+smdubarry@users.noreply.github.com>
2025-05-17 16:08:08 -0500

    Stop logging eating

--- src/villager.js
+++ src/villager.js
@@ -320,7 +320,7 @@ export function stepVillager(v, index, ticks, log) {
             if (tile.type === 'house' && tile.stored > 0) {
                 tile.stored--;
                 v.health = 100;
-                if (log) log(`${v.name} ate at ${tile.name}`);
+                // if (log) log(`${v.name} ate at ${tile.name}`);
                 releaseGatherTarget(v);
                 v.task = null;
                 v.target = null;
